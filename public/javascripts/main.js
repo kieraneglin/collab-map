9c776b9187815359ee4069f6bda512c0aee3b532
Kieran Eglin <kieran.eglin@gmail.com>
2017-08-19 19:49:34 -0700

    added support for multiple dynamic rooms

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,19 @@
-"use strict";
+'use strict';
 
-document.addEventListener("DOMContentLoaded", function () {
-  var mouse = {
-    click: false,
-    move: false,
-    pos: {
-      x: 0,
-      y: 0
-    },
-    pos_prev: false
-  };
+document.addEventListener('DOMContentLoaded', function () {
+  var room = window.location.href.split('/').pop(); // This is cheeky and gross and I hate myself for it
+  var id = Math.floor(Math.random() * 9e15); // We need to ignore our own messages, so we need an ID.  This is also weak, but so is my will to live
+  var socket = io.connect();
+
+  socket.on('connect', function () {
+    socket.emit('room', room);
+  });
+
+  socket.on('line', function (data) {
+    console.log('Incoming line:', data);
+  });
+
+  setTimeout(function () {
+    socket.emit('draw_line', { room: room, message: 'HEY' });
+  }, 3000);
 });
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,11 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let mouse = {
-    click: false,
-    move: false,
-    pos: {
-      x: 0,
-      y: 0
-    },
-    pos_prev: false
-  };
+document.addEventListener('DOMContentLoaded', () => {
+  const room = window.location.href.split('/').pop(); // This is cheeky and gross and I hate myself for it
+  const id = Math.floor(Math.random() * 9e15); // We need to ignore our own messages, so we need an ID.  This is also weak, but so is my will to live
+  const socket = io.connect();
+
+  socket.on('connect', () => {
+     socket.emit('room', room);
+  });
+
+  socket.on('line', (data) => {
+     console.log('Incoming line:', data);
+  });
+
+  setTimeout(() => {
+    socket.emit('draw_line', { room: room, message: 'HEY'});
+  }, 3000);
 });
